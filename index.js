677d10d3b7278d64136529b739769e7f6e7f0b9d
Long ZIP <longlv@v-auto.com.vn>
2019-10-02 15:44:58 +0700

    test

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-require("dotenv").config(); // Sets up dotenv as soon as our application starts
+//server.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const favicon = require("express-favicon");
 const path = require("path");
-
-const router = express.Router();
-
+const port = process.env.PORT || 8080;
 const app = express();
-
+const router = express.Router();
+app.use(favicon(__dirname + "/build/favicon.ico"));
 // Middleware
 app.use(bodyParser.json());
 app.use(
@@ -17,24 +17,15 @@ app.use(
 );
 app.use(cors());
 
-// This is equivalent to
-app.use("/", bodyParser.json());
-
-// if (environment !== "production") {
-//   const logger = require("morgan");
-//   app.use(logger("dev"));
-//   app.use("/", logger("dev"));
-// }
-
 const routes = require("./routes/index.js");
-
 app.use("/api/v1", routes(router));
-// app.use(express.static(__dirname));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/.*/", (req, res) =>
-  res.sendFile(path.join(__dirname, "build", "index.html"))
-);
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+app.listen(port);
